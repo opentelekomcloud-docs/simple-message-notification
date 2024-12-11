8e8a3dc74fbc9cf112b071294fcef816b9f7fb0b
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2024-12-11 10:58:50 +0100

    Update tox.ini && conf.py file (#40)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'smn-api-ref.tex',
-     u'Simple Message Notification - API Reference',
+    u'Simple Message Notification - API Reference',
      u'OpenTelekomCloud', 'manual'),
 ]
 
