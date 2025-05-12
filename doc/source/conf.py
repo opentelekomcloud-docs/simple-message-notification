19734e1b3a40a080e07d6c2df9e42c7b52b32e5e
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2025-05-12 15:12:03 +0200

    Update tox.ini && conf.py file (#49)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,9 @@ otcdocs_doc_type = ''
 otcdocs_service_category = 'application'
 otcdocs_service_title = 'Simple Message Notification'
 otcdocs_service_type = 'smn'
+otcdocs_cloud_environment = 'eu_de'
 otcdocs_search_environment = 'hc_de'
+otcdocs_search_index = 'search_index_de'
 otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
